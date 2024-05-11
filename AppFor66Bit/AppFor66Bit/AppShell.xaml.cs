6dd1209636c7c1d75332f98ce8c5b5fd982c0f29
maakhhh <d.makhlonov@mail.ru>
2024-05-11 17:43:30 +0500

    Initial commit

---
+++ AppFor66Bit/AppFor66Bit/AppShell.xaml.cs
@@ -0,0 +1,17 @@
+﻿using AppFor66Bit.ViewModels;
+using AppFor66Bit.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace AppFor66Bit
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ AppFor66Bit/AppFor66Bit/ViewModels/ItemsViewModel.cs
@@ -0,0 +1,53 @@
+﻿using AppFor66Bit.Models;
+using AppFor66Bit.Views;
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppFor66Bit.ViewModels
+{
+    public class ItemsViewModel : BaseViewModel
+    {
+
+        public ObservableCollection<Item> Items { get; }
+        public Command LoadItemsCommand { get; }
+
+        public ItemsViewModel()
+        {
+            Title = "Избранное";
+            Items = new ObservableCollection<Item>();
+            LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
+        }
+
+        async Task ExecuteLoadItemsCommand()
+        {
+            IsBusy = true;
+
+            try
+            {
+                Items.Clear();
+                var items = await DataStore.GetItemsAsync(true);
+                foreach (var item in items)
+                {
+                    Items.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        public void OnAppearing()
+        {
+            IsBusy = true;
+        }
+
+    }
+}
